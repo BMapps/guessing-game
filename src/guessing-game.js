76ebd494b8e14468f578c6f41c776c87f4e874d9
BMapps <mihael.09k@gmail.com>
2019-10-10 21:41:44 +0300

    add: completed task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,20 +2,27 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min=min;
+        this.max=max;
+        this.result;
     }
 
     guess() {
+        this.result=Math.round((this.max-this.min)/2+this.min);
+        return this.result;
 
     }
 
     lower() {
+        this.max=this.result;
 
     }
 
     greater() {
+        this.min=this.result;
 
     }
 }
+
 
 module.exports = GuessingGame;
